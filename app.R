c67b9aa7fc6e4864b75fda764f5200e7351aed66
mbhammond <mbhammond@bren.ucsb.edu>
2022-02-01 19:46:47 -0800

    Commit post data input

--- app.R
+++ app.R
@@ -4,9 +4,12 @@ library(palmerpenguins)
 
 
 ui <- fluidPage(
-  titlePanel("Title Here"),
+  titlePanel("NOT TOO SHABBY AY"),
   sidebarLayout(
     sidebarPanel("widgets here",
+                 selectInput("select", label = h5("Select box"),
+                             choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+                             selected = 1),
                  dateRangeInput("dates",
                                 label = h5("Date Range")
                                 ),
@@ -15,7 +18,8 @@ ui <- fluidPage(
                                     selected = 1),
                  radioButtons("radio", label = h5("Radio Buttons"),
                               choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
-                              selected = 1)
+                              selected = 1),
+                 
     ), 
     #end widgets
     mainPanel("Graph here")
